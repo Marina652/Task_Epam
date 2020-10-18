07f38fdee5a42c97caf7f0404d8ec6b23f09765b
Marina652 <73078809+Marina652@users.noreply.github.com>
2020-10-18 23:17:39 +0300

    Add files via upload

---
+++ task/ConsoleApp1/ConsoleApp1/Circle.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    class Circle : GeometricalFigure
+    {
+        public Circle(params double[] arr)
+        {
+            this.type = "Круг";
+            this.CharacteristicsOfFigures = new double[1];
+            
+            for (int i = 0; i < arr.Length; i++)
+                CharacteristicsOfFigures[i] = arr[i];
+        }
+
+        public override double GetArea()
+        {
+            return CharacteristicsOfFigures[0] * CharacteristicsOfFigures[0] * System.Math.PI;
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * CharacteristicsOfFigures[0] * System.Math.PI;
+        }
+    }
+}
---
+++ task/ConsoleApp1/ConsoleApp1/GeometricalFigure.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    abstract class GeometricalFigure
+    {
+        abstract public double GetArea();
+        abstract public double GetPerimeter();
+
+        protected double [] CharacteristicsOfFigures;
+        protected string type;
+        public string Type => type;
+    }
+}
